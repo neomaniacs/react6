3fe5fc4a3e89f8d906bbd1d2fda827881bdd92b7
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2022-12-29 23:03:36 +0100

    last updates

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import styled from './HeaderCartButton.module.css' ;
+
+import React from 'react';
+
+const HeaderCartButton = (props) => {
+    return <button>
+        <span>
+            
+        </span>
+        <span></span>
+        <span></span>
+    </button>
+} ;
+
+export default HeaderCartButton ;
